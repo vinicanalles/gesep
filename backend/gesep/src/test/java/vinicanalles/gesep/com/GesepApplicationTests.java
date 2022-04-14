893fc7466435bbfe6de2ceda52598548a9e3e565
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-04-13 22:58:27 -0300

    First Commit

---
+++ backend/gesep/src/test/java/vinicanalles/gesep/com/GesepApplicationTests.java
@@ -0,0 +1,13 @@
+package vinicanalles.gesep.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
