ef0a711b8ad1b9c726a8db9f5d63818c8f2ba3b4
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-04-17 21:44:58 -0300

    Documentos TCC

--- backend/gesep/src/main/java/vinicanalles/gesep/com/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package vinicanalles.gesep.com.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import vinicanalles.gesep.com.core.domain.entities.Endereco;
-
-public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
-}
